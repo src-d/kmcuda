8adc482f18ffb38f5db73a884c68b27a463d6e4e
Aleksandrs Pavlenko <pavnexus@gmail.com>
2018-12-06 15:38:45 +0200

    removed " __attribute__((unused))" as it is not being supported on Win

--- src/metric_abstraction.h
+++ src/metric_abstraction.h
@@ -169,7 +169,7 @@ struct METRIC<kmcudaDistanceMetricCosine, F> {
   }
 
   FPATTR static typename HALF<F>::type distance(
-      F sqr1 __attribute__((unused)), F sqr2 __attribute__((unused)), F prod) {
+      F sqr1, F sqr2, F prod) {
     float fp = _float(_fin(prod));
     if (fp >= 1.f) return _half<F>(0.f);
     if (fp <= -1.f) return _half<F>(M_PI);
@@ -252,7 +252,7 @@ struct METRIC<kmcudaDistanceMetricCosine, F> {
     return acos(partial);
   }
 
-  FPATTR static void normalize(uint32_t count __attribute__((unused)), float *vec) {
+  FPATTR static void normalize(uint32_t count, float *vec) {
     // Kahan summation with inverted c
     float norm = 0, corr = 0;
     #pragma unroll 4
@@ -272,7 +272,7 @@ struct METRIC<kmcudaDistanceMetricCosine, F> {
   }
 
   #if CUDA_ARCH >= 60
-  FPATTR static void normalize(uint32_t count __attribute__((unused)), half2 *vec) {
+  FPATTR static void normalize(uint32_t count, half2 *vec) {
     // We really have to calculate norm in 32-bit floats because the maximum
     // value which 16-bit float may represent is 2^16.
     float norm = 0, corr = 0;
